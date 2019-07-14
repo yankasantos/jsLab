c67682d9677a2106fa11a81415b2f91e360c092c
yankasantos <yankasantos0107@gmail.com>
2019-07-14 18:36:21 -0300

    estrutura inicial, montando uma menssagem para exibir no console

---
+++ developers.js
@@ -0,0 +1,11 @@
+var endereco = {
+    rua: "Rua dos pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+};
+
+function informacaoDev(endereco) {
+    console.log("O usuário mora em "+ endereco.cidade + "/"+ endereco.uf+" , no bairro"+ endereco.bairro+", na rua " +  endereco.rua +" com nº "+endereco.numero+".")
+}
