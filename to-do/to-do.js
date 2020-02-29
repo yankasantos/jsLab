eb78bf8d42f65f8ad05eb7dce57c61c5e9675fa1
yankasantos <yankasantos0107@gmail.com>
2020-02-29 18:13:29 -0300

    implementando a funcionalidade de adicionar item na lista e deletando logicamente

--- to-do/to-do.js
+++ to-do/to-do.js
@@ -9,12 +9,37 @@ var toDos = [
 ];
 
 function renderToDos(){
+    listElement.innerHTML = '';
     for(toDo of toDos){
         var toDoElement = document.createElement('li');
         var toDoTesxtElement = document.createTextNode(toDo);
+        var linkElement = document.createElement('a');
+        
+        
+        linkElement.setAttribute('href', '#');
+        linkText = document.createTextNode('Excluir');
+        linkElement.appendChild(linkText);
+        
+        var pas = toDos.indexOf(toDo);
+        linkElement.setAttribute('onclick', 'deleteToDo(' + pas + ')')
 
         toDoElement.appendChild(toDoTesxtElement);
+        toDoElement.appendChild(linkElement);
         listElement.appendChild(toDoElement);
     }
 }
 renderToDos();
+
+function addToDo(){
+    var toDoText = inputElement.value;
+    toDos.push(toDoText);
+    inputElement.value = '';
+    renderToDos();
+}
+
+buttonElement.onclick = addToDo;
+
+function deleteToDo(pas){
+    toDos.splice(pas,1);
+    renderToDos();
+}
