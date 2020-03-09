3f9c7e2932724f8c329a35c1b3c5f26d11827078
yankasantos <yankasantos0107@gmail.com>
2020-03-09 13:27:56 -0300

    adicionando uma função assincrona obtendo o status da requisição a api e retornando os valores o transformando em json

--- modulo4-JS-assincrono/main.js
+++ modulo4-JS-assincrono/main.js
@@ -2,3 +2,9 @@ var requestGithubUser = new XMLHttpRequest();
 
 requestGithubUser.open('GET', 'https://api.github.com/users/yankasantos')
 requestGithubUser.send(null);
+
+requestGithubUser.onreadystatechange = function(){
+    if(requestGithubUser.readyState === 4){
+        console.log(JSON.parse(requestGithubUser.responseText));
+    }
+}
