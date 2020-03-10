7371e5f24d89c43a591bb60b3c72dc520d9fe0f3
yankasantos <yankasantos0107@gmail.com>
2020-03-10 11:10:45 -0300

    utilizando o Axios para fazer as promises de requisições HTTP onde já converte o retorno em JSON

--- modulo4-JS-assincrono/main.js
+++ modulo4-JS-assincrono/main.js
@@ -1,25 +1,6 @@
-var myFirstPromise= function(){
-    return new Promise(function(resolve, reject){
-
-        var requestGithubUser = new XMLHttpRequest();
-
-        requestGithubUser.open('GET', 'https://api.github.com/users/yankasantos');
-        requestGithubUser.send(null);
-
-        requestGithubUser.onreadystatechange = function(){
-            if(requestGithubUser.readyState === 4){
-                if(requestGithubUser.status ===200){
-                resolve(JSON.parse(requestGithubUser.responseText));
-                } else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
-
-myFirstPromise()
+axios.get('https://api.github.com/users/yankasantos')
     .then(function(response){
+        console.log(response.data.avatar_url);
         console.log(response);
     })
 
