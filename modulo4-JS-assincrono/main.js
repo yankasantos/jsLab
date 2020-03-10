67487898274d04391824da816efcaf29f68fe9c5
yankasantos <yankasantos0107@gmail.com>
2020-03-10 10:57:30 -0300

    utilizando promise para fazer a requisição assincrona com tratamento de erro

--- modulo4-JS-assincrono/main.js
+++ modulo4-JS-assincrono/main.js
@@ -1,10 +1,31 @@
-var requestGithubUser = new XMLHttpRequest();
+var myFirstPromise= function(){
+    return new Promise(function(resolve, reject){
 
-requestGithubUser.open('GET', 'https://api.github.com/users/yankasantos')
-requestGithubUser.send(null);
+        var requestGithubUser = new XMLHttpRequest();
 
-requestGithubUser.onreadystatechange = function(){
-    if(requestGithubUser.readyState === 4){
-        console.log(JSON.parse(requestGithubUser.responseText));
-    }
+        requestGithubUser.open('GET', 'https://api.github.com/users/yankasantos');
+        requestGithubUser.send(null);
+
+        requestGithubUser.onreadystatechange = function(){
+            if(requestGithubUser.readyState === 4){
+                if(requestGithubUser.status ===200){
+                resolve(JSON.parse(requestGithubUser.responseText));
+                } else {
+                    reject('Erro na requisição');
+                }
+            }
+        }
+    });
 }
+
+myFirstPromise()
+    .then(function(response){
+        console.log(response);
+    })
+
+
+    .catch(function(error){
+        console.warn(error); 
+    });
+
+
