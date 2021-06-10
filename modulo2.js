2b8ef382f76e74d87628d8319a549910dfca142d
Yanka Santos <yankasantos0107@gmail.com>
2021-06-10 11:34:08 -0300

    Update modulo2.js
    Trocando o var para const e feichando o cochete[]

--- modulo2.js
+++ modulo2.js
@@ -6,23 +6,23 @@
 *        </div>
 */
 
-var inputElement =  document.querySelector('input[name=nome');  
-var inputElementAll =  document.querySelectorAll('input');  
+const inputElement =  document.querySelector('input[name=nome]');  
+const inputElementAll =  document.querySelectorAll('input');  
 
 console.log(inputElement);
 console.log(inputElementAll);   
 
-var inputElementValue = document.querySelector('input[name=nome]');
-var btnElement = document.querySelector('button.botao');
+const inputElementValue = document.querySelector('input[name=nome]');
+const btnElement = document.querySelector('button.botao');
 btnElement.onclick = function(){
     alert('Botão clicado');
 
 }    
 
-var linkElement = document.createElement('a');
+const linkElement = document.createElement('a');
 linkElement.setAttribute('href', 'https://github.com/yankasantos');
-var textElement = document.createTextNode('Conheça o meu Github ^⁻^');
+const textElement = document.createTextNode('Conheça o meu Github ^⁻^');
 linkElement.appendChild(textElement);
 
-var containerElement=document.querySelector('#app');
+const containerElement=document.querySelector('#app');
 containerElement.appendChild(linkElement);
