b1530820a8b078cae7129ac723164474789abeab
yankasantos <yankasantos0107@gmail.com>
2024-08-15 23:04:01 -0300

    JS basic concepts following the freeCodeCamp course

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # jsLab
-Esse repositório terá alguns exercícios de **JavaScript**,
-baseado no curso da __Rockeatseat__, e vai servir como um guia 
-para comandos do básico ao avançado de **JavaScript**.
+Esse repositório terá alguns exercícios de **JavaScript**
---
+++ algorithms-data-structures/pyramid-generator/index.js
@@ -0,0 +1,31 @@
+//01 - console.log() - render sominting on console
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("    o   ");
+console.log("   ooo  ");
+console.log("  ooooo ");
+console.log(" ooooooo");
+console.log("ooooooooo ");
+
+//02 - variable
+let character;
+
+//03 - using variable
+let character = "Hello";
+
+//04 - using single quotes
+let character = 'Hello';
+
+//05 - showing the variable value
+let character = 'Hello';
+console.log(character);
+
+//06 - reusing variable changing the value
+let character = 'Hello';
+console.log(character);
+character = "World";
+
+//07 - showing agail the variable value
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
